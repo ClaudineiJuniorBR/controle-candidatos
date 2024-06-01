6e635cf5e3064e0d1eeae04f3f9b74eb4fff2de3
Junior Martins <claudineijunior1996@gmail.com>
2024-06-01 17:29:58 -0300

    new

--- ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,51 +1,51 @@
-package candidatura;
+package candidatura; //com a pasta candidatura criamos um package
 
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random; //importa o pacote random
+import java.util.concurrent.ThreadLocalRandom; //pra definir um valor entre dois pontos
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         System.out.println("######################################################################");
         String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "MIRELA", "DANIELA" };
-        for (String candidato : candidatos) {
-            entrandoEmContato(candidato);
+        for (String candidato : candidatos) { // forma simplicada de mostrar cada candidato
+            entrandoEmContato(candidato); // chama o metodo
         }
-
     }
 
-    static void entrandoEmContato(String candidato) {
-        int tentativasRealizadas = 1;
-        boolean continuarTentando = true;
-        boolean atendeu = false;
+    static void entrandoEmContato(String candidato) { // criação do metodo
+        int tentativasRealizadas = 1;// cria a variavel
+        boolean continuarTentando = true; // cria a variavel bool
+        boolean atendeu = false; // cria a variavel bool
         do {
             // elas precisaram sofrer alterações
             // senão vai entrar em um loop infinito
-            atendeu = atender();
-            continuarTentando = !atendeu;
-            if (continuarTentando)
-                tentativasRealizadas++;
+            atendeu = atender(); // passsa pra variavel atendeu o valor do metodo atender
+            continuarTentando = !atendeu; // quando for 0 ele para, inverte atendeu
+            if (continuarTentando) // codnição se for 0, quando vai tentar 3 vezes
+                tentativasRealizadas++; // soma um
             else
-                System.out.println("CONTATO REALIZADO COM SUCESSO");
+                System.out.println("CONTATO REALIZADO COM SUCESSO"); // caso atenda
+        } while (continuarTentando && tentativasRealizadas < 3);// se continuar e tentativa for true ele continua por
+                                                                // tres vezes
 
-        } while (continuarTentando && tentativasRealizadas < 3);
-        if (atendeu) {
+        if (atendeu) { // condição para caso atender
             System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " Tentativa");
-        } else {
+        } else { // caso não atendeu
             System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", Número máximo de tentativas "
                     + tentativasRealizadas);
         }
     }
 
-    static boolean atender() {
-        return new Random().nextInt(3) == 1;
+    static boolean atender() { // criação do metodo
+        return new Random().nextInt(3) == 1; // random o valor
     }
 
-    static void imprimirSelecionados() {
+    static void imprimirSelecionados() { // para imprimir os candidatos
         String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
-                "DANIELA", "JORGE" };
+                "DANIELA", "JORGE" }; // lista de candidatos
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
 
-        for (int indice = 0; indice < candidatos.length; indice++) {
+        for (int indice = 0; indice < candidatos.length; indice++) { // vai de 0 até o numero de candidatos
             System.out.println("O candidato de nº " + (indice + 1) + " é " + candidatos[indice]);
         }
         System.out.println("Forma abreviada de interação for each");
@@ -54,36 +54,36 @@ public class ProcessoSeletivo {
         }
     }
 
-    static void selecaoCandidatos() {
+    static void selecaoCandidatos() { // seleção de candidatos por conta do salario
         String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
                 "DANIELA", "JORGE" };
         int candidaotsSelecionados = 0;
         int candidatosAtual = 0;
-        double salarioBase = 2000.00;
+        double salarioBase = 2000.00; // defini o salario base
         while (candidaotsSelecionados < 5 && candidatosAtual < candidatos.length) {
             String candidato = candidatos[candidatosAtual];
             double salarioPretendido = valorPretendido();
             System.out.println("----------------------------------------------------------------");
             System.out.println("O candidato " + candidato + " Solicitou este valor de salário R$ " + salarioPretendido);
 
-            if (salarioBase >= salarioPretendido) {
+            if (salarioBase >= salarioPretendido) { // condição para salario
                 System.out.println("----------------------------------------------------------------");
                 System.out.println("O candidato " + candidato + " Foi selecionado para a vaga com um salário de R$ "
                         + salarioPretendido);
-                candidaotsSelecionados++;
+                candidaotsSelecionados++; // quantidade de selecioandos aumenta com esse incrementador
             }
-            candidatosAtual++;
+            candidatosAtual++;// quantidade de atuais aumenta com esse incrementador
         }
     }
 
-    static double valorPretendido() {
-        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    static double valorPretendido() { // gera um valor random entre 1800 até 2200
+        return ThreadLocalRandom.current().nextDouble(1800, 2200); // retorna o valor
     }
 
-    static void analisarCandidaro(double salarioPretendido) {
-        double salarioBase = 2000.00;
+    static void analisarCandidaro(double salarioPretendido) { // criação do metodo
+        double salarioBase = 2000.00; // salario base
         try {
-            if (salarioBase > salarioPretendido) {
+            if (salarioBase > salarioPretendido) { // codição para recrutar candidato
                 System.out.println("----------------------------------------------------------------");
                 System.out.println("LIGAR PARA O CANDIDATO. ");
             } else if (salarioBase == salarioPretendido) {
